92ff3defca53cf1354be6a1bc14c7c469e0efad9
kokolybe <52601492+kokolybe@users.noreply.github.com>
2024-06-30 12:01:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1934,7 +1934,7 @@ async function ai(redfox, event) {
                 sendMessage(redfox, event, handleError({ stacktrace: err, cuid: redfox.getCurrentUserID(), e: event }));
             }
             */
-            const url = "https://deku-rest-api-ywad.onrender.com/gpt4?prompt=";
+            const url = "https://joshweb.click/gpt4?prompt=";
             const prompt = data.join(" ");
             const uid = event.senderID;
             try {
